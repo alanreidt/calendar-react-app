37c591ffa806c98e73aa6ae17c74698cd83cdfb1
Alan Reid <alanreidt@gmail.com>
2020-07-28 12:57:34 +0300

    Add App mock data

--- src/index.js
+++ src/index.js
@@ -4,9 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const DAY_ENTRIES = [
+  {
+    date: "2020-06-10",
+    tasks: [
+      { name: "Пробежка", time: "08:00" },
+      { name: "Завтрак", time: "09:00" },
+      { name: "Совещание", time: "12:00" },
+    ],
+  },
+  {
+    date: "2020-06-29",
+    tasks: [
+      { name: "Завтрак", time: "09:00" },
+      { name: "Спортзал", time: "19:00" },
+    ],
+  },
+  {
+    date: "2020-06-31",
+    tasks: [
+      { name: "Обед", time: "13:30" },
+      { name: "Скайпкол", time: "17:40" },
+    ],
+  },
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App dayEntries={DAY_ENTRIES} />
   </React.StrictMode>,
   document.getElementById('root')
 );
