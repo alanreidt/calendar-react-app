e3d3d58fa9a6e96386dc87b976f6ba1df79e1249
Alan Reid <alanreidt@gmail.com>
2020-07-30 11:13:38 +0300

    Simplify the app data mock

--- src/index.js
+++ src/index.js
@@ -4,34 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const DAY_ENTRIES = [
-  {
-    date: "2020-06-10",
-    tasks: [
-      { name: "Пробежка", time: "08:00" },
-      { name: "Завтрак", time: "09:00" },
-      { name: "Совещание", time: "12:00" },
-    ],
-  },
-  {
-    date: "2020-06-29",
-    tasks: [
-      { name: "Завтрак", time: "09:00" },
-      { name: "Спортзал", time: "19:00" },
-    ],
-  },
-  {
-    date: "2020-06-31",
-    tasks: [
-      { name: "Обед", time: "13:30" },
-      { name: "Скайпкол", time: "17:40" },
-    ],
-  },
+// the date is stored in ISO format (Monday is at 0th index) and in UTC time zone
+const WEEK_TASKS = [
+  [
+    { name: "Пробежка", time: "05:00" },
+    { name: "Завтрак", time: "06:00" },
+    { name: "Совещание", time: "09:00" },
+  ],
+  [],
+  [
+    { name: "Завтрак", time: "06:00" },
+    { name: "Спортзал", time: "16:00" },
+  ],
+  [],
+  [
+    { name: "Обед", time: "10:30" },
+    { name: "Скайпкол", time: "14:40" },
+  ],
+  [],
+  [],
 ];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App dayEntries={DAY_ENTRIES} />
+    <App weekTasks={WEEK_TASKS} />
   </React.StrictMode>,
   document.getElementById('root')
 );
