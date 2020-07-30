23bdd70964a49a8aeb0dbdca9394c33f58cc3775
Alan Reid <alanreidt@gmail.com>
2020-07-30 12:19:54 +0300

    Add Tabs component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Tabs } from 'antd';
+import moment from 'moment';
+
 import './App.css';
 
-function App() {
+const { TabPane } = Tabs;
+
+const DAY_NAMES = [
+  'Вс',
+  'Пн',
+  'Вт',
+  'Ср',
+  'Чт',
+  'Пт',
+  'Сб',
+];
+
+function App(props) {
+  const weekTasks = props.weekTasks;
+  const todayDayIndex = moment().day();
+
   return (
     <div className="App">
+      <Tabs defaultActiveKey={`${todayDayIndex}`} tabPosition="right">
+        {weekTasks.map((dayTasks, i) => (
+          <TabPane tab={DAY_NAMES[i]} key={i}>
+            Content of tab {i}
+            {dayTasks[0] && dayTasks[0].name}
+          </TabPane>
+        ))}
+      </Tabs>
     </div>
   );
 }
