8c9e54be6570c73ffc663a18ed9aa5b1c8916bbb
Alan Reid <alanreidt@gmail.com>
2020-07-30 10:35:40 +0300

    Create complex structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import { Tabs } from 'antd';
+import moment from 'moment';
+
 import './App.css';
 
-function App() {
+const { TabPane } = Tabs;
+
+const DAY_NAMES = [
+  'Вс',
+  'Пн',
+  'Вт',
+  'Ср',
+  'Чт',
+  'Пт',
+  'Сб',
+];
+
+function App(props) {
+  const now = moment();
+  const todayDayIndex = now.getDay();
+  // const todayDateISO = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`;
+
+  const getCurrentWeekDates = () => {
+    const now = moment();
+    const todayDayIndex = now.getDay();
+
+    const date = new Date(now.getFullYear, now.getMonth, now.getDate);
+    const firstWeekDay = date.setDate(date.getDate - todayDayIndex);
+
+    const WEEK_DAYS_NUMBER = 7;
+    const currentWeekDates = [];
+
+    for (let i = 0; i < 7; i++) {
+      // const dateISO =
+
+      // currentWeekDates.push
+    }
+  };
+
+  // const currentWeekDatesISO = getCurrentWeekDates();
+
   return (
     <div className="App">
+      <Tabs defaultActiveKey={`${todayDayIndex}`} tabPosition="right">
+        {DAY_NAMES.map((dayName, i) => {
+          // const dayEntry = props.schedule[i];
+          // const {dateISO, tasks} = dayEntry;
+          // const date = new Date(Date.parse(dateISO));
+          // const dayNameIndex = date.getDay();
+          // const dayName = DAY_NAMES[dayNameIndex];
+
+          return (
+            <TabPane tab={dayName} key={i}>
+              Content of tab {i}
+              {/* {if ()} */}
+            </TabPane>
+          )
+        })}
+      </Tabs>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,34 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const DAY_ENTRIES = [
-  {
-    date: "2020-06-10",
-    tasks: [
-      { name: "Пробежка", time: "08:00" },
-      { name: "Завтрак", time: "09:00" },
-      { name: "Совещание", time: "12:00" },
-    ],
-  },
-  {
-    date: "2020-06-29",
-    tasks: [
-      { name: "Завтрак", time: "09:00" },
-      { name: "Спортзал", time: "19:00" },
-    ],
-  },
-  {
-    date: "2020-06-31",
-    tasks: [
-      { name: "Обед", time: "13:30" },
-      { name: "Скайпкол", time: "17:40" },
-    ],
-  },
-];
+const SCHEDULE = {
+  "2020-06-10": [
+    { name: "Пробежка", timeISO: "08:00" },
+    { name: "Завтрак", timeISO: "09:00" },
+    { name: "Совещание", timeISO: "12:00" },
+  ],
+  "2020-06-29": [
+    { name: "Завтрак", timeISO: "09:00" },
+    { name: "Спортзал", timeISO: "19:00" },
+  ],
+  "2020-06-31": [
+    { name: "Обед", timeISO: "13:30" },
+    { name: "Скайпкол", timeISO: "17:40" },
+  ],
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App dayEntries={DAY_ENTRIES} />
+    <App schedule={SCHEDULE} />
   </React.StrictMode>,
   document.getElementById('root')
 );
