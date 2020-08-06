475a456b7c36bcd301731e06bf007bf6dd842f4f
Alan Reid <alanreidt@gmail.com>
2020-08-06 12:12:57 +0300

    Set up TaskList component fields key

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-import { Form, TimePicker, Input, Button, Space } from 'antd';
+import { Form, TimePicker, Input, Button, Space, InputNumber } from 'antd';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 
 const TIME_FORMAT = "HH:mm";
 
 const TaskList = ({ initialTasks = [], handleFinish, index }) => {
   const [form] = Form.useForm();
+
+  const hasOwnID = (index) => initialTasks[index] && initialTasks[index].id !== undefined;
+  const getOwnID = (index) => initialTasks[index].id;
+  const generateID = (index) => index + 1;
+  const getID = (index) => hasOwnID(index) ? getOwnID(index) : generateID(index);
 
   const onFinish = ({ tasks }) => {
     const sortedTasks = tasks.sort((a, b) => a.time - b.time);
@@ -27,8 +32,8 @@ const TaskList = ({ initialTasks = [], handleFinish, index }) => {
         {(fields, { add, remove }) => {
           return (
             <div>
-              {fields.map((field) => (
-                <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="start">
+              {fields.map((field, index) => (
+                <Space key={getID(index)} style={{ display: 'flex', marginBottom: 8 }} align="start">
                   <Form.Item
                     {...field}
                     name={[field.name, 'time']}
@@ -44,6 +49,15 @@ const TaskList = ({ initialTasks = [], handleFinish, index }) => {
                     rules={[{ required: true, message: 'Missing a task name' }]}
                   >
                     <Input placeholder="Введите задачу" allowClear />
+                  </Form.Item>
+                  <Form.Item
+                    {...field}
+                    name={[field.name, 'id']}
+                    fieldKey={[field.fieldKey, 'id']}
+                    initialValue={hasOwnID(index) ? undefined : generateID(index)}
+                    hidden
+                  >
+                    <InputNumber />
                   </Form.Item>
 
                   <MinusCircleOutlined
