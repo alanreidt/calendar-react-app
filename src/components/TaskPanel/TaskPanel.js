275d75178a838e75c596002cb9df4772490fb86c
Alan Reid <alanreidt@gmail.com>
2020-08-12 18:45:12 +0300

    Make TaskPanel component similar to TaskList

--- src/components/TaskPanel/TaskPanel.js
+++ src/components/TaskPanel/TaskPanel.js
@@ -1,61 +1,67 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { v4 as uuid } from 'uuid';
 
 import { Form, Button } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import Task from '../Task/Task';
 
+const generateID = (index) => uuid();
+
 const TaskPanel = ({ id, onTaskPanelFinish }) => {
-  const [shown, setShown] = useState(false);
-
-  const generateID = (index) => uuid();
-
-  const initialValues = {
-    date: '',
-    name: '',
-    id: generateID(),
-  };
-
-  const handleAddButtonClick = () => {
-    setShown(true);
-  };
-
   const [form] = Form.useForm();
-  const handleFinish = (newTask) => {
+  const handleFinish = ({ tasks }) => {
+    const newTask = tasks[0];
+    console.log(newTask);
+
     onTaskPanelFinish(newTask, id);
 
-    form.setFieldsValue(initialValues);
-
-    setShown(false);
+    form.resetFields();
   };
-
-  const getID = (index) => initialValues && initialValues.id;
 
   return (
     <div className="TaskPanel">
       <Form
-        name="task-panel-form"
         form={form}
+        name="task-panel-form"
         onFinish={handleFinish}
-        initialValues={initialValues}
         autoComplete="off"
       >
-        <Task
-          id={getID()}
-          button={
-            <PlusOutlined
-              onClick={handleAddButtonClick}
-            />
-          }
-        />
-        <Form.Item hidden>
-          <Button
-            type="primary"
-            htmlType="submit"
-          >
-            Submit
-          </Button>
-        </Form.Item>
+        <Form.List name="tasks">
+          {(fields, { add }) => {
+            return (
+              <div>
+                {fields.map((field, index) => (
+                  <Task
+                    key={generateID(index)}
+                    id={generateID(index)}
+                    index={index}
+                    button={
+                      <Form.Item>
+                        <Button htmlType="submit">
+                          <PlusOutlined />
+                        </Button>
+                      </Form.Item>
+                    }
+                  />
+                ))}
+
+                {fields.length <= 0 &&
+                  <Form.Item>
+                    <Button
+                      type="dashed"
+                      onClick={() => {
+                        add();
+                      }}
+                      block
+                    >
+                      <PlusOutlined /> Add field
+                    </Button>
+                  </Form.Item>
+                }
+              </div>
+            );
+          }}
+        </Form.List>
       </Form>
     </div>
   );
