ff903e71870d2d512518eac9437027dd3953ffd7
Alan Reid <alanreidt@gmail.com>
2020-09-22 13:04:46 +0300

    Build a production code into a docs folder

---
+++ docs/precache-manifest.ce61f16cebf0f40e21f8f36476baa0f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e80da56c306ac5f7c8436ab4daa667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84a6e06455940fee7fbd",
+    "url": "/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "e422c9cc3ce01922e915",
+    "url": "/static/css/main.3ec30336.chunk.css"
+  },
+  {
+    "revision": "84a6e06455940fee7fbd",
+    "url": "/static/js/2.903a4059.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.903a4059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e422c9cc3ce01922e915",
+    "url": "/static/js/main.7a395925.chunk.js"
+  },
+  {
+    "revision": "a61f406248462318648d",
+    "url": "/static/js/runtime-main.b505d36f.js"
+  }
+]);
