3375243e844dcdc2a9ebb3598398c6222b9462df
Alan Reid <alanreidt@gmail.com>
2020-09-22 13:34:24 +0300

    Updates

---
+++ precache-manifest.5571b03c0f937691ccfcbf216e66bf4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a30bd801d856079d12ec04985d53ec",
+    "url": "/calendar-react-app/index.html"
+  },
+  {
+    "revision": "b4c6aba54d3453f8722e",
+    "url": "/calendar-react-app/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "1090f4320ada5372cffc",
+    "url": "/calendar-react-app/static/css/main.3ec30336.chunk.css"
+  },
+  {
+    "revision": "b4c6aba54d3453f8722e",
+    "url": "/calendar-react-app/static/js/2.b3f732ff.chunk.js"
+  },
+  {
+    "revision": "082c1bdf24861af2babbd4591bc978fa",
+    "url": "/calendar-react-app/static/js/2.b3f732ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1090f4320ada5372cffc",
+    "url": "/calendar-react-app/static/js/main.48b6726e.chunk.js"
+  },
+  {
+    "revision": "3b956f01903df263d678",
+    "url": "/calendar-react-app/static/js/runtime-main.e42512d6.js"
+  }
+]);
